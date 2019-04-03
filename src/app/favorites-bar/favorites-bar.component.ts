42f72ee51bad1b73f3197312506dd41f5d7d1526
Student <example@example.com>
2019-04-03 16:30:20 -0700

    Add favorites bar component

---
+++ src/app/favorites-bar/favorites-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-bar',
+  templateUrl: './favorites-bar.component.html',
+  styleUrls: ['./favorites-bar.component.scss']
+})
+export class FavoritesBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
