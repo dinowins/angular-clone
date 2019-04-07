35a4c16ce1bbc5a5d74827e415d69bbb596a7367
Dino Nguyen <dino@>
2019-04-07 13:53:43 -0700

    Change routing position for signup and login to be in app root html

---
+++ src/app/header/header.routing.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './../login/login.component';
+import { SignupComponent } from './../signup/signup.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
