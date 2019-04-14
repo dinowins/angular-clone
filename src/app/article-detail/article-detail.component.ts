3d0eda8d6ec52c864cbff7f662d0ba86b8e8158b
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-14 14:00:42 -0700

    Add dynamic routing for retreiving parameters to article-detail comp

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-detail.component.scss']
 })
 export class ArticleDetailComponent implements OnInit {
+  articleId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.articleId = parseInt(urlParameters['id']);
+    })
   }
 
 }
